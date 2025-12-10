e521a983ac9742d8a1c116cef06d445dc7eb4cf6
priscilapaolamh-lang <priscilapaolamh@gmail.com>
2025-12-09 19:08:56 -0500

    Create App.java

---
+++ src/main/java/src/main/java/App.java
@@ -0,0 +1,10 @@
+// Archivo: src/main/java/App.java
+public class App {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+        int x = 5;
+        if (x > 3) {
+            System.out.println("x es mayor que 3");
+        }
+    }
+}
